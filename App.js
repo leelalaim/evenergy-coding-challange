1ebc53605de5bfde2ed4982d046c62cf9b47c4eb
Leelalaim <leelalaim@gmail.com>
2021-10-18 21:49:25 +0200

    added spinner

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { SearchPage } from "./pages/Search";
-import { HomePage } from "./pages/Home";
+import { SearchPage } from "./pages/SearchPage";
+import { HomePage } from "./pages/HomePage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
+import { ActivityIndicator } from "react-native";
 
 import { Card } from "../components/Card";
 import { Search } from "../components/Search";
+import { Loader } from "../components/Loader";
 
 export const SearchPage = ({ country }) => {
   const [search, setSearch] = useState("");
   const [stations, setStations] = useState([]);
   const [filteredStations, setFilteredStations] = useState([]);
-  const [showAll, setShowAll] = useState(true);
+  const [showAll, setShowAll] = useState(true)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetchData();
@@ -19,7 +22,10 @@ export const SearchPage = ({ country }) => {
       `https://api.openchargemap.io/v3/poi/?output=json&countrycode=GB&compact=true&verbose=false&key='e05ac04b-3ec5-436b-893a-978da4b706d1'`
     )
       .then((res) => res.json())
-      .then((data) => setStations(data))
+      .then((data) => {
+        setStations(data)
+        setLoading(false)
+      })
       .catch((err) => alert(`Error: ${err}`))
   };
 
@@ -42,20 +48,6 @@ export const SearchPage = ({ country }) => {
     }
   };
 
-  // const charge = (id) => {
-  //   fetch("https://example.ev.energy/chargingsession", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       user: 1,
-  //       car_id: 1,
-  //       charger_id: { id },
-  //     }),
-  //   }).then((res) => console.log(res));
-  // };
-
   return (
     <SearchConatiner>
       <Search
@@ -64,26 +56,27 @@ export const SearchPage = ({ country }) => {
         value={search}
         placeholder="Search here"
       />
+      {loading && <Loader />}
       {showAll &&
         stations.map((station, index) => {
           return (
+            <>
             <Card
-              key={index}
               title={station.AddressInfo.Title}
               address={station.AddressInfo.AddressLine1}
               postcode={station.AddressInfo.Postcode}
               town={station.AddressInfo.Town}
               btnText="Charge"
             />
+            </>
           );
         })}
       <CardWrapper>
         {filteredStations &&
           !showAll &&
-          filteredStations.map((station, index) => {
+          filteredStations.map((station) => {
             return (
               <Card
-                key={index}
                 title={station.AddressInfo.Title}
                 address={station.AddressInfo.AddressLine1}
                 postcode={station.AddressInfo.Postcode}
